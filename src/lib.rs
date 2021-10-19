099afd5e9d5035549fa1aa9a3a6e39dd85a20fed
Frank Denis <github@pureftpd.org>
2021-10-19 23:53:36 +0200

    Doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+//! # benchmark-simple
+//!
+//! A tiny benchmarking library for Rust.
+//!
+//! - Trivial to use
+//! - Works pretty much everywhere, including WebAssembly (WASI, but also in-browser)
+//!
+//! ```rust
+//! use benchmark_simple::*;
+//!
+//! fn test_function() {
+//!     // ...
+//! }
+//!
+//! let bench = Bench::new();
+//! let res = bench.run(None, || test_function());
+//! println!("result: {}", res);
+//! ```
+//!
+//! Throughput computation:
+//!
+//! ```rust
+//! use benchmark_simple::*;
+//!
+//! fn test_function(m: &mut [u8]) {
+//!     // ...
+//! }
+//!
+//! let mut m = vec![0u8; 1_000_000];
+//! let bench = Bench::new();
+//! let res = bench.run(None, || test_function(&mut m));
+//! let throughput = res.throughput(m.len() as _);
+//! println!("throughput: {}", throughput);
+//! ```
+
 use precision::*;
 use std::fmt::{self, Debug, Display, Formatter};
 use std::mem;
